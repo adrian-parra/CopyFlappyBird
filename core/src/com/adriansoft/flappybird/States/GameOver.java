465349110b9c977e2ac0970a8be897981a9eaa7d
adrian <adrian.adrianborquez@gmail.com>
2022-12-05 11:13:53 -0700

    Semodifico el estado gameOver ahora se maneja el nombre del usuario y se muestra el nombre de usuario con el record mas alto.

--- core/src/com/adriansoft/flappybird/States/GameOver.java
+++ core/src/com/adriansoft/flappybird/States/GameOver.java
@@ -6,18 +6,28 @@ import com.adriansoft.flappybird.States.PlayState;
 import com.adriansoft.flappybird.States.State;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class GameOver extends State {
-    private Texture background,gameover,playbutton,ground,muerte;
+    private Texture background,gameover,playbutton,ground,muerte,puntaje;
     private String scoreName;
     private String recordName;
     private BitmapFont scorefontname,recordfontname;
     private int score,record;
     private int newrecord,oldreco;
+
+    private Stage stage;
+
+    private String user ,userActual;
 
 
 
@@ -28,8 +38,15 @@ public class GameOver extends State {
         newrecord = preferences.getInteger("newrecord", 0);
         oldreco = preferences.getInteger("oldrecord", 0);
 
+        user = preferences.getString("user","");
+        userActual = preferences.getString("userActual","anonimo");
+
         background = new Texture("bg.png");
         playbutton = new Texture("playbtn.png");
+        puntaje = new Texture("puntaje.png");
+
+
+
         gameover = new Texture("gameover.png");
         ground = new Texture("ground.png");
         muerte =  new Texture("m3.png");
@@ -45,6 +62,7 @@ public class GameOver extends State {
 
     @Override
     protected void handleInput() {
+
         if (Gdx.input.justTouched()){
             gsm.set(new PlayState(gsm));
             gsm.setScore(0);
@@ -67,27 +85,67 @@ public class GameOver extends State {
 */
 
 
-        scoreName = "Tu puntage es de: " + score;
+        scoreName = ""+user +" su puntaje fue de "+ score;
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
         FlappyBird.stopMusica();
         spriteBatch.draw(background,0,0,FlappyBird.WIDTH , FlappyBird.HEIGHT);
+
+
         spriteBatch.draw(playbutton ,camera.position.x - playbutton.getWidth() / 2,camera.position.y);
+
         spriteBatch.draw(gameover,camera.position.x - gameover.getWidth() /2,camera.position.y + (gameover.getHeight() * 2) );
-        scorefontname.setColor(Color.BLUE);
-        scorefontname.getData().setScale(1,1);
-        scorefontname.draw(spriteBatch ,scoreName,camera.position.x /2  , camera.position.y + (gameover.getHeight() * 4));
+        //scorefontname.setColor(Color.BLUE);
+        //scorefontname.getData().setScale(1,1);
+       //spriteBatch.draw(puntaje,camera.position.x /2  , camera.position.y + (gameover.getHeight() * 4));
+        //scorefontname.draw(spriteBatch ,scoreName,camera.position.x /2  , (camera.position.y - camera.position.y) +30);
+       // scorefontname.draw(spriteBatch ,scoreName,30 , 30);
+
+
         spriteBatch.draw(ground,0,0);
 
-        if (score > oldreco){
-            recordfontname.setColor(Color.BLUE);
-            recordfontname.getData().setScale(1,1);
-            recordfontname.draw(spriteBatch ,"conseguiste nuevo record: " + newrecord ,20,20);
+        scorefontname.draw(spriteBatch ,scoreName,20,40);
+
+        if (score >= oldreco && score == newrecord){
+            //recordfontname.setColor(Color.BLUE);
+            //recordfontname.getData().setScale(1,1);
+            //recordfontname.draw(spriteBatch ,"Conseguiste nuevo record: " + newrecord ,20,20);
+            /*
+            if(score == newrecord){
+
+            }else {
+                recordfontname.draw(spriteBatch ,"conseguiste nuevo record" ,20,20);
+            }*/
+            if(oldreco == newrecord){
+                if(user.equals(userActual)){
+                    recordfontname.draw(spriteBatch , "tienes un record de " + newrecord ,20,20);
+
+                }else{
+                    recordfontname.draw(spriteBatch ,""+userActual +" tiene un record de " + newrecord ,20,20);
+
+                }
+            }else{
+                recordfontname.draw(spriteBatch ,"conseguiste nuevo record" ,20,20);
+            }
+
 
         } else{
-            recordfontname.setColor(Color.BLUE);
-            recordfontname.getData().setScale(1,1);
-            recordfontname.draw(spriteBatch ,"record a superar es de: " + newrecord ,20,20);
+            //recordfontname.setColor(Color.BLUE);
+            //recordfontname.getData().setScale(1,1);
+
+            if(userActual.equals("anonimo")){
+                recordfontname.draw(spriteBatch ,"sin record registrado" ,20,20);
+            }else{
+
+                if(user.equals(userActual)){
+                    recordfontname.draw(spriteBatch , "tienes un record de " + newrecord ,20,20);
+
+                }else{
+                    recordfontname.draw(spriteBatch ,""+userActual +" tiene un record de " + newrecord ,20,20);
+
+                }
+
+            }
         }
 
         //spriteBatch.draw(muerte ,78,0);
@@ -95,6 +153,7 @@ public class GameOver extends State {
         spriteBatch.end();
 
     }
+
 
 
     @Override
